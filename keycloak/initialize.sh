6a96b5aa8cdb1e277afed25b23e10fc5f309c818
senjin-hajrulahovic <senjin.hajrulahovic@gmail.com>
2019-05-30 01:11:51 +0200

    refactor keycloak initialization and add postman collection

--- keycloak/initialize.sh
+++ keycloak/initialize.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
+#enter container
 docker exec -it <keycloak-container-id> /bin/bash
+
 cd keycloak/bin/
 
+# authenticate admin user
 ./kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user admin --password admin
 
 # create user
-./kcadm.sh create users -r media_realm -s username=media_user -s enabled=true
+USER_ID=$(./kcadm.sh create users -r media_realm -s username=media_user -s enabled=true -i)
 # created user with id <user-id>
 
 # need to create client roles
 ./kcadm.sh create clients/f914278a-1208-4b7a-a212-19058db6d7ed/roles -r media_realm -s name=media_user
 
 #update use password
-./kcadm.sh update users/bd7c0aa4-60fb-4fdc-bc74-140ebd8f54e7/reset-password -r media_realm -s type=password -s value=media_user -s temporary=false -n
+./kcadm.sh update users/${USER_ID}/reset-password -r media_realm -s type=password -s value=media_user -s temporary=false -n
 
 #add role to user
 ./kcadm.sh add-roles -r media_realm --uusername=media_user --cclientid rest_media_client_id --rolename media_user
