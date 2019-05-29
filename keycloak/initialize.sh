9abddacaa0037f0e9d857845ad1d9c2a1847f0c0
senjin-hajrulahovic <senjin.hajrulahovic@gmail.com>
2019-05-30 00:25:34 +0200

    working bearer token resolving

--- keycloak/initialize.sh
+++ keycloak/initialize.sh
@@ -8,8 +8,8 @@ cd keycloak/bin/
 
 ./kcadm.sh create users -r media_realm -s username=media_user -s enabled=true
 # create user with id <user-id>
-./kcadm.sh update users/<user-id>/reset-password -r media_realm -s type=password -s value=media_user -s temporary=false -n
+./kcadm.sh update users/b64487b8-4441-421c-9bb6-839bc2a961de/reset-password -r media_realm -s type=password -s value=media_user -s temporary=false -n
 ./kcadm.sh add-roles -r media_realm --uusername=media_user --rolename user
 
 # acquire token
-curl -d 'client_id=public_media_client_id' -d 'username=media_user' -d 'password=password' -d 'grant_type=password' 'http://localhost:8080/auth/realms/media_realm/protocol/openid-connect/token' | python -m json.tool
+curl -d 'client_id=public_media_client_id' -d 'username=media_user' -d 'password=media_user' -d 'grant_type=password' 'http://localhost:8080/auth/realms/media_realm/protocol/openid-connect/token' | python -m json.tool
--- src/main/java/com/hardcodedlambda/media/config/SecurityConfig.java
+++ src/main/java/com/hardcodedlambda/media/config/SecurityConfig.java
@@ -7,19 +7,23 @@ import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticatio
 import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
 import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @KeycloakConfiguration
-//@EnableWebSecurity
+@EnableWebSecurity
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
     @Autowired
@@ -53,10 +57,50 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         super.configure(http);
         http.authorizeRequests()
-                .antMatchers("/*").hasRole("user")
-                .anyRequest().permitAll()
+//                .antMatchers("*").hasRole("user")
+                .anyRequest().hasRole("MEDIA_USER")
                 .and().csrf().disable();
 
+    }
+
+
+    ////
+
+    @Bean
+    public GrantedAuthoritiesMapper grantedAuthoritiesMapper() {
+        SimpleAuthorityMapper mapper = new SimpleAuthorityMapper();
+        mapper.setConvertToUpperCase(true);
+        return mapper;
+    }
+
+    @Override
+    protected KeycloakAuthenticationProvider keycloakAuthenticationProvider() {
+        final KeycloakAuthenticationProvider provider = super.keycloakAuthenticationProvider();
+        provider.setGrantedAuthoritiesMapper(grantedAuthoritiesMapper());
+        return provider;
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    /////
+
+    @Bean
+    public FilterRegistrationBean keycloakAuthenticationProcessingFilterRegistrationBean(
+            final KeycloakAuthenticationProcessingFilter filter) {
+        final FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+        registrationBean.setEnabled(false);
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean keycloakPreAuthActionsFilterRegistrationBean(
+            final KeycloakPreAuthActionsFilter filter) {
+        final FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+        registrationBean.setEnabled(false);
+        return registrationBean;
     }
 
 }
