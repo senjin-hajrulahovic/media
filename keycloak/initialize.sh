730ac349a14add7f2e71b89856641b31345369f1
senjin-hajrulahovic <senjin.hajrulahovic@gmail.com>
2019-05-30 00:54:22 +0200

    include keycloak in docker-compose.yml

--- keycloak/initialize.sh
+++ keycloak/initialize.sh
@@ -5,11 +5,18 @@ cd keycloak/bin/
 
 ./kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user admin --password admin
 
-
+# create user
 ./kcadm.sh create users -r media_realm -s username=media_user -s enabled=true
-# create user with id <user-id>
-./kcadm.sh update users/b64487b8-4441-421c-9bb6-839bc2a961de/reset-password -r media_realm -s type=password -s value=media_user -s temporary=false -n
-./kcadm.sh add-roles -r media_realm --uusername=media_user --rolename user
+# created user with id <user-id>
+
+# need to create client roles
+./kcadm.sh create clients/f914278a-1208-4b7a-a212-19058db6d7ed/roles -r media_realm -s name=media_user
+
+#update use password
+./kcadm.sh update users/bd7c0aa4-60fb-4fdc-bc74-140ebd8f54e7/reset-password -r media_realm -s type=password -s value=media_user -s temporary=false -n
+
+#add role to user
+./kcadm.sh add-roles -r media_realm --uusername=media_user --cclientid rest_media_client_id --rolename media_user
 
 # acquire token
 curl -d 'client_id=public_media_client_id' -d 'username=media_user' -d 'password=media_user' -d 'grant_type=password' 'http://localhost:8080/auth/realms/media_realm/protocol/openid-connect/token' | python -m json.tool
